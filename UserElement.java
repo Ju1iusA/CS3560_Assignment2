5e63b9a5b4695b9085713611c1e16f9632bcb6eb
Ju1iusA <juliusaguilar03@gmail.com>
2026-07-03 21:06:29 -0700

    Update UserElement.java
    Updated for Assignment 3

--- UserElement.java
+++ UserElement.java
@@ -1,5 +1,8 @@
 package MiniTwitter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //Interface that tracks users and groups
 interface UserElement {
     void visitUser(User user);
@@ -12,10 +15,30 @@ class AnalysisVisitor implements UserElement {
     private int groupCount = 0;
     private int messageCount = 0;
     private int positiveMessageCount = 0;
+    
+    //Variables for tracking ID rules and last updated user data
+    private final Set<String> uniqueIds = new HashSet<>();
+    private boolean containsInvalidIds = false;
+    private long latestTimestamp = -1;
+    private String lastUpdatedUser = "None";
 
     //Analyzes user and their tweets
     public void visitUser(User user) {
         userCount++;
+        
+        //ID Verification Check for spaces and duplicates
+        String currentId = user.getId();
+        if (currentId.contains(" ") || uniqueIds.contains(currentId)) {
+            containsInvalidIds = true;
+        }
+        uniqueIds.add(currentId);
+        
+        //Last updated user check
+        if (user.getLastUpdateTime() > latestTimestamp) {
+            latestTimestamp = user.getLastUpdateTime();
+            lastUpdatedUser = currentId;
+        }
+
         for (String tweet : user.getNewsFeed()) {
             messageCount++;
             String textOnly = tweet.substring(tweet.indexOf(":") + 1).toLowerCase();
@@ -30,6 +53,13 @@ class AnalysisVisitor implements UserElement {
         if (!group.getId().equals("Root")) {
             groupCount++;
         }
+        
+        //ID Verification Check
+        String currentId = group.getId();
+        if (currentId.contains(" ") || uniqueIds.contains(currentId)) {
+            containsInvalidIds = true;
+        }
+        uniqueIds.add(currentId);
     }
 
     //Getters for data
@@ -44,5 +74,13 @@ class AnalysisVisitor implements UserElement {
     }
     public double getPositivePercentage() { 
         return messageCount == 0 ? 0.0 : ((double) positiveMessageCount / messageCount) * 100.0; 
+    }
+    
+    public boolean areAllIdsValid() {
+        return !containsInvalidIds;
+    }
+    
+    public String getLastUpdatedUser() {
+        return lastUpdatedUser;
     }
 }
